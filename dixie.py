f41dc0b00aeeb1e54803b5da28f7baa9dc4db031
Salman Shuaib <salman.shuaib@gmail.com>
2023-06-13 02:34:39 -0400

    :muscle: dixie compiles cannon fodder for ChatGPT

--- dixie.py
+++ dixie.py
@@ -1,9 +1,8 @@
 '''
 1. PUT dixie.py AT THE ROOT OF THE FOLDER STRUCTURE YOU WANT TO SEARCH, I.E. ONE LEVEL ABOVE THE FOLDER YOU WANT TO SEARCH.
-2. REPLACE FOLDER NAME "frisky" IN dixie.py WITH THE FOLDER YOU WANT TO SEARCH: SUCH AS "flask".
-3. RUN dixie.py.
-4. ENTER THE NAMES OF THE FILES YOU WANT TO SEARCH FOR, SEPARATED BY COMMAS (e.g., cont.html, jessie.py, random.js).
-5. A TEXT FILE "file_contents.txt" WILL BE GENERATED THAT YOU CAN USE TO CONVERSE IN CODE WITH ChatGPT.
+2. RUN dixie.py.
+3. ENTER THE NAMES OF THE FILES YOU WANT TO SEARCH FOR, SEPARATED BY COMMAS (e.g., cont.html, jessie.py, random.js).
+4. A TEXT FILE "file_contents.txt" WILL BE GENERATED THAT YOU CAN USE TO CONVERSE IN CODE WITH ChatGPT.
 '''
 
 
@@ -28,9 +27,14 @@ def write_contents_to_file(file_contents):
             f.write(f"**{file}:\n{content}\n\n")
 
 def submit_form():
-    folder_path = os.path.join(os.getcwd(), 'flask')
+    folder_name = folder_entry.get()
+    folder_path = os.path.join(os.getcwd(), folder_name)
     requested_files = entry.get()
     requested_files = [file.strip() for file in requested_files.split(",")]
+
+    if not os.path.isdir(folder_path):
+        messagebox.showerror("Error", f"Folder '{folder_name}' does not exist.")
+        return
 
     file_contents = search_files(folder_path, requested_files)
     write_contents_to_file(file_contents)
@@ -42,11 +46,17 @@ window = tk.Tk()
 window.title("File Search")
 window.geometry("600x200")  # Adjusted the window size
 
+folder_label = tk.Label(window, text="Enter the folder name to search in:")
+folder_label.pack()
+
+folder_entry = tk.Entry(window, width=60)
+folder_entry.pack(pady=5)
+
 label = tk.Label(window, text="Enter the file names (separated by commas):")
 label.pack()
 
 entry = tk.Entry(window, width=60)
-entry.pack(pady=20)
+entry.pack(pady=10)
 
 submit_button = tk.Button(window, text="Submit", command=submit_form)
 submit_button.pack()
